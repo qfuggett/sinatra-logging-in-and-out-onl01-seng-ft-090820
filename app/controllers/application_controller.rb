3748a1c05cf1956a4032f9c6b010a73d3416af94
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-23 21:29:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     binding.pry
     if @user = User.find_by(username[params [:username]])
-      session[:user_id] = @user.id
+      session[:session_id] = @user.id
       redirect to "/account"
     else
       erb :error
