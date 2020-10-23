546fdc6b48dece3d263073b15bc1d1b8bdaf00ac
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-23 21:47:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(params)
-    if @user 
+    if @user && @user.password == params[:password]
       session[:user_id] = @user.id
       redirect "/account"
     else
